a0a082403070428aceb1b105edda3721d7d84dfb
Dawid <66019090+ASSYNU@users.noreply.github.com>
2023-03-12 12:51:05 +0100

    Timetable: Revert timetable changes

--- Vulcanova/Vulcanova/Features/Timetable/TimetableChangeColorConverter.cs
+++ Vulcanova/Vulcanova/Features/Timetable/TimetableChangeColorConverter.cs
@@ -12,19 +12,6 @@ public class TimetableChangeColorConverter : IMultiValueConverter
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         var value = values.FirstOrDefault();
-        
-        if (value is TimetableListEntry)
-        {
-            var entry = (TimetableListEntry)values.FirstOrDefault();
-            var nowDate = DateTime.Now;
-            var lessonDate = entry.Date;
-            var startDate = entry.Start;
-            var endDate = entry.End;
-            if(nowDate.Date == lessonDate.Date && startDate.TimeOfDay < nowDate.TimeOfDay && nowDate.TimeOfDay < endDate.TimeOfDay)
-                return ThemeUtility.GetThemedColorByResourceKey("PrimaryColor");
-            
-            value = entry.Change;
-        }
 
         if (value is TimetableListEntry.ChangeDetails change)
         {
